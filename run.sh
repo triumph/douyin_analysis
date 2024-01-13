e1b36f560c30a7e5bba32559f5b0f06cc3f9882d
stzzWang <1050100468@qq.com>
2024-01-14 00:13:13 +0800

    抖音短视频无水印的解析服务

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 cd /root
 #wget https://github.com/STZZ-1992/douyin_analysis/releases/latest/download/douyin_analysis.tar.gz -O /root/douyin_analysis.tar.gz
+mkdir /root/douyin_analysis
 tar -zxvf douyin_analysis.tar.gz -C ./douyin_analysis
 cd douyin_analysis
 pip install -r requirements.txt
