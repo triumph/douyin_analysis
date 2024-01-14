cf3a329d51943c280905372a8f46d3363a1818cc
stzzWang <1050100468@qq.com>
2024-01-14 08:08:10 +0800

    抖音短视频无水印的解析服务

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@ cd /root
 mkdir douyin_analysis
 tar -zxvf ./douyin_analysis.tar.gz -C ./douyin_analysis
 cd ./douyin_analysis
+docker rmi douyin_analysis-douyin_analysis:latest
 docker compose down
 docker compose up -d
